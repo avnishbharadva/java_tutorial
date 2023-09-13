420bcdcab30643b7cbc838fe2bc6a0d119ca5645
Avnish Bharadva <avnishbharadva@gmail.com>
2023-09-13 19:15:14 +0530

    Static Block Demo

---
+++ Staticblockdemo.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Staticblockdemo {
+    static int count=0;
+    String name;
+    int m1,m2,m3,total;
+    float per;
+
+    static{
+        System.out.println("Static Block Called...");
+    }
+
+    Staticblockdemo(){
+        System.out.println("Default Constructor Called...");
+        name = null;
+        m1 = 0;
+        m2 = 0;
+        m3 = 0;
+        total = 0;
+        per = 0.0f;
+        count += 1;
+    }
+
+    void add(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Name : ");
+        name = sc.next();
+        System.out.print("Enter Marks 1 : ");
+        m1 = sc.nextInt();
+        System.out.print("Enter Marks 2 : ");
+        m2 = sc.nextInt();
+        System.out.print("Enter Marks 3 : ");
+        m3 = sc.nextInt();
+        sc.close();
+        calcper();
+    }
+
+    void calcper(){
+        total = m1 + m2 + m3;
+        per = total * 0.3f;
+    }
+
+    void show(){
+        System.out.println("Name : " + name);
+        System.out.println("Marks 1 : " + m1);
+        System.out.println("Marks 2 : " + m2);
+        System.out.println("Marks 3 : " + m3);
+        System.out.println("Total : " + total);
+        System.out.println("Percentage : " + per);
+        System.out.println("Total Objects Created : " + count);
+    }
+
+    {
+        System.out.println("Non-static Block Called...");
+    }
+    public static void main(String[] args) {
+        Staticblockdemo obj = new Staticblockdemo();
+        obj.add();
+        obj.show();
+
+        Staticblockdemo o2 = new Staticblockdemo();
+        o2.show();
+    }    
+}
