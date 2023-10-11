ea351789a6b6262fc06e240d09222345fa3c5561
Avnish Bharadva <avnishbharadva@gmail.com>
2023-10-11 23:44:34 +0530

    Diamond Pattern

--- Diamond_pattern.java
+++ Diamond_pattern.java
@@ -1,28 +1,45 @@
-public class Diamond_pattern {
-    public static void main(String[] args) {
-        for(int i=1;i<=6;i++){
-            for(int j=5;j>=i;j--){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("1");
-            }
-            for(int j=1;j<=i-1;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print(" ");
-            }
-            for(int j=5;j>=i;j--){
-                System.out.print("*");
-            }
-            for(int j=4;j>=i;j--){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
+import java.util.Scanner;
+
+public class Diamond_pattern
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter Any Odd Number : ");
+		int n = sc.nextInt();
+
+        	for(int i=1;i<=(n/2+1);i++)
+		{
+            		for(int j=(n/2);j>=i;j--)
+			{
+                		System.out.print("  ");
+            		}
+            		for(int j=1;j<=i;j++)
+			{
+                		System.out.print("* ");
+            		}
+            		for(int j=1;j<=i-1;j++)
+			{
+                		System.out.print("* ");
+            		}
+            		System.out.println();
+        	}
+        	for(int i=1;i<=(n/2);i++)
+		{
+            		for(int j=1;j<=i;j++)
+			{
+                		System.out.print("  ");
+            		}
+            		for(int j=(n/2);j>=i;j--)
+			{
+                		System.out.print("* ");
+            		}
+            		for(int j=(n/2-1);j>=i;j--)
+			{
+                		System.out.print("* ");
+            		}
+            		System.out.println();
+        	}
+	}
 }
