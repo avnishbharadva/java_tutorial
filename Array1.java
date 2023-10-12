0448ede93098876b8fd578c6f4c53e9a546ee889
Avnish Bharadva <avnishbharadva@gmail.com>
2023-10-12 23:57:53 +0530

    Array 1

---
+++ Array1.java
@@ -0,0 +1,19 @@
+class Array1
+{
+	public static void main(String args[])
+	{
+		//int arr[];
+		//arr = new int[1];
+
+		int arr[] = {1,2,3,4};
+		//int arr2[] = new int[]{10,20,30,40};
+		 
+		int arr2[] = new int[3];
+		//arr[0] = 10;
+		//arr[1] = 20;
+		//arr[2] = 30;
+
+		//for(int i=0;i<arr2.length;i++)
+		System.out.println(arr.length);
+	}
+}
